2a6e2fca5b1a68d71cc7464cabfc809219a3dc3d
João Alberto <joaoams6@gmail.com>
2025-07-11 16:34:18 -0300

    início

---
+++ script.js
@@ -0,0 +1,17 @@
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+  direction: "vertical",
+  gestureDirection: "vertical",
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
